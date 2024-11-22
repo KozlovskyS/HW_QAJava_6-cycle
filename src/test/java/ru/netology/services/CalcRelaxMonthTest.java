dc9519bc0b5c554537298722e4c4e79727f99b09
KozlovskyS <skozmail@gmail.com>
2024-11-22 22:52:54 +0300

    Create parameterized test

--- src/test/java/ru/netology/services/CalcRelaxMonthTest.java
+++ src/test/java/ru/netology/services/CalcRelaxMonthTest.java
@@ -1,27 +1,27 @@
 package ru.netology.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CalcRelaxMonthTest {
 
-    @Test
-    public void calculateMonthLittle() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/relax.csv")
+    public void testCalculateMonth(int expected, int income, int expenses, int threshold) {
         CalcRelaxMonth service = new CalcRelaxMonth();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        //System.out.println("Всего " + totalMonthRelax + " месяца(ев) отдыха за год");
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    public void calculateMonthBig() {
-        CalcRelaxMonth service = new CalcRelaxMonth();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        //System.out.println("Всего " + totalMonthRelax + " месяца(ев) отдыха за год");
-        Assertions.assertEquals(expected, actual);
-    }
+// @Test
+// public void calculateMonthBig() {
+//     CalcRelaxMonth service = new CalcRelaxMonth();
+//
+//     int expected = 2;
+//     int actual = service.calculate(100_000, 60_000, 150_000);
+//     Assertions.assertEquals(expected, actual);
+// }
 }
