657b3362b4ab58774a1457a35d6cd5ddae344efe
KozlovskyS <skozmail@gmail.com>
2024-11-22 22:19:49 +0300

    Create tests

--- src/test/java/ru/netology/services/CalcRelaxMonthTest.java
+++ src/test/java/ru/netology/services/CalcRelaxMonthTest.java
@@ -1,14 +1,27 @@
 package ru.netology.services;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 public class CalcRelaxMonthTest {
 
     @Test
-    public void calculateMonth() {
+    public void calculateMonthLittle() {
         CalcRelaxMonth service = new CalcRelaxMonth();
 
-        int totalMonthRelax = service.calculate(10_000, 3_000, 20_000);
-        System.out.println("Всего " + totalMonthRelax + " месяца(ев) отдыха за год");
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        //System.out.println("Всего " + totalMonthRelax + " месяца(ев) отдыха за год");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateMonthBig() {
+        CalcRelaxMonth service = new CalcRelaxMonth();
+
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        //System.out.println("Всего " + totalMonthRelax + " месяца(ев) отдыха за год");
+        Assertions.assertEquals(expected, actual);
     }
 }
